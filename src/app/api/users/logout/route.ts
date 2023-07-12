8f9af2353754ea94e829bff42b328b9e7569342e
Prankul <vprankul@gmail.com>
2023-07-12 12:29:06 +0530

    process of adding logout route

---
+++ src/app/api/users/logout/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const response = NextResponse.json({
+      message: "Logout successful",
+      success: true,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
